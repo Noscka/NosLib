2fc9cf077c3d691b59283af717d0627fa87d2dd8
Noscka <Adam882200@gmail.com>
2022-10-20 18:46:35 +0100

    Adding more functions

---
+++ NosStdLib/NosStdLib/FileManagement.hpp
@@ -0,0 +1,71 @@
+#ifndef _FILEMANAGEMENT_HPP_
+#define _FILEMANAGEMENT_HPP_
+
+#include <string>
+#include <filesystem>
+
+/*
+Contains:
+
+ - FilePathStorage class
+*/
+
+namespace NosStdLib
+{
+    namespace FileManagement
+    {
+        /// <summary>
+        /// Class which allows for storing file paths
+        /// </summary>
+        class FilePath
+        {
+        private:
+            std::wstring AbsolutePath; /* Absolute path to exe (without exe filename) */
+            std::wstring RelativePath; /* Subpath from current path */
+            std::wstring Filename; /* Filename */
+
+            FilePath() {}
+        public:
+            /// <summary>
+            /// Create FilePath Object
+            /// </summary>
+            /// <param name="relativePath">- path from current/absolute path</param>
+            /// <param name="filename">- filename</param>
+            FilePath(std::wstring relativePath, std::wstring filename)
+            {
+                RelativePath = relativePath;
+                Filename = filename;
+                AbsolutePath = std::filesystem::current_path().wstring();
+            }
+
+            /// <summary>
+            /// Returns Absolute path without filename
+            /// </summary>
+            /// <returns>Absolute path without filename</returns>
+            std::wstring GetAbsolutePath()
+            {
+                return AbsolutePath + RelativePath;
+            }
+
+            /// <summary>
+            /// Returns Absolute path to file
+            /// </summary>
+            /// <returns>Absolute path to file</returns>
+            std::wstring GetFilePath()
+            {
+                return GetAbsolutePath() + Filename;
+            }
+
+            /// <summary>
+            /// Return just name of file
+            /// </summary>
+            /// <returns>filename</returns>
+            std::wstring GetFilename()
+            {
+                return Filename;
+            }
+        };
+    }
+}
+
+#endif
--- NosStdLib/NosStdLib/Global.hpp
+++ NosStdLib/NosStdLib/Global.hpp
@@ -2,40 +2,76 @@
 #define _GLOBAL_HPP_
 
 #include <string>
-#include <filesystem>
+#include <stringapiset.h>
+#include <Windows.h>
+
+/*
+Contains:
+
+String
+    - to_wstring
+    - to_string
+*/
+
 
 namespace NosStdLib
 {
-    class FilePathStorage
+    namespace Global
     {
-    private:
-        std::wstring AbsolutePath;
-        std::wstring SubPath;
-        std::wstring Filename;
-
-        FilePathStorage() {}
-    public:
-        FilePathStorage(std::wstring subPath, std::wstring filename)
+        namespace String
         {
-            SubPath = subPath;
-            Filename = filename;
-            AbsolutePath = std::filesystem::current_path().wstring();
+            /// <summary>
+            /// Converts string to wstring
+            /// </summary>
+            /// <param name="str">- string for converting</param>
+            /// <returns>wstring version of the string</returns>
+            std::wstring ToWstring(const std::string& str)
+            {
+                if (str.empty()) return std::wstring();
+                int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+                std::wstring wstrTo(size_needed, 0);
+                MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+                return wstrTo;
+            }
+
+            /// <summary>
+            /// Converts wstring to string
+            /// </summary>
+            /// <param name="wstr">- wstring for converting</param>
+            /// <returns>string version of the string</returns>
+            std::string ToString(const std::wstring& wstr)
+            {
+                if (wstr.empty()) return std::string();
+                int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+                std::string strTo(size_needed, 0);
+                WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+                return strTo;
+            }
         }
 
-        std::wstring GetSubPath()
+        namespace Console
         {
-            return AbsolutePath + SubPath;
-        }
+            void ClearRange(int position, int range, wchar_t fillChar = L' ')
+            {
+                CONSOLE_SCREEN_BUFFER_INFO csbi;
+                HANDLE ConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-        std::wstring GetFilePath()
-        {
-            return GetSubPath() + Filename;
-        }
+                COORD tl = { 0, (SHORT)(position) };
+                GetConsoleScreenBufferInfo(ConsoleHandle, &csbi);
+                DWORD written, cells = csbi.dwSize.X * (1 + range);
+                FillConsoleOutputCharacter(ConsoleHandle, fillChar, cells, tl, &written);
+                FillConsoleOutputAttribute(ConsoleHandle, csbi.wAttributes, cells, tl, &written);
+                SetConsoleCursorPosition(ConsoleHandle, tl);
+            }
 
-        std::wstring GetFileName()
-        {
-            return Filename;
+            void ClearScreen(wchar_t fillChar = L' ')
+            {
+                CONSOLE_SCREEN_BUFFER_INFO csbi;
+                HANDLE ConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+                GetConsoleScreenBufferInfo(ConsoleHandle, &csbi);
+                ClearRange(0, csbi.dwSize.Y, fillChar);
+            }
         }
-    };
+    }
 }
 #endif
