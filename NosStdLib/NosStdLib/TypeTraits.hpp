656f9b17c41d986e8c2250e3a1a3337e1cec193e
Noscka <Adam882200@gmail.com>
2023-02-05 20:40:42 +0000

    Added TypeTrait that checks if datatype is a character

--- NosStdLib/NosStdLib/TypeTraits.hpp
+++ NosStdLib/NosStdLib/TypeTraits.hpp
@@ -11,6 +11,13 @@ namespace NosStdLib
 	namespace TypeTraits
 	{
 		/// <summary>
+		/// Specifies what ::value will return
+		/// </summary>
+		template<bool ReturnValue>
+		struct TypeTraitReturn { static const bool value = ReturnValue; };
+
+	#pragma region remove_all_pointers
+		/// <summary>
 		/// Goes to core type
 		/// </summary>
 		/// <typeparam name="T">- type to remove pointers from</typeparam>
@@ -30,6 +37,34 @@ namespace NosStdLib
 		/// <typeparam name="T">- type to remove pointers from</typeparam>
 		template<typename T>
 		using remove_all_pointers_t = typename remove_all_pointers<T>::type;
+	#pragma endregion
+
+	#pragma region is_character
+		/// <summary>
+		/// Checks if datatype is a character datatype
+		/// </summary>
+		template<typename T>
+		struct is_character : TypeTraitReturn<false> {};
+
+		template<>
+		struct is_character<char> : TypeTraitReturn<true> {};
+
+		template<>
+		struct is_character<wchar_t> : TypeTraitReturn<true> {};
+
+		template<>
+		struct is_character<char8_t> : TypeTraitReturn<true> {};
+
+		template<>
+		struct is_character<char16_t> : TypeTraitReturn<true> {};
+
+		template<>
+		struct is_character<char32_t> : TypeTraitReturn<true> {};
+
+		template<typename T>
+		constexpr bool is_character_v = is_character<T>::value;
+	#pragma endregion
+
 
 	}
 }
