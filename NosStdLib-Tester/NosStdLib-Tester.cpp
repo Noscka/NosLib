83e5c685e3843e698263ba36e8883566f0cc7df8
Adam Krawczyk <Adam882200@gmail.com>
2023-03-06 09:26:30 +0000

    Got a working none blocking input. need to make it work

--- NosStdLib-Tester/NosStdLib-Tester.cpp
+++ NosStdLib-Tester/NosStdLib-Tester.cpp
@@ -13,28 +13,82 @@
 
 /* TODO: Figure out if it is worth it to change calling convention from default (__cdelc) to __fastcall */
 
-bool SomeBool = false;
-int number = 1;
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
 
-void CheckBool()
+std::condition_variable cv{};
+std::mutex mtx;
+std::queue<wchar_t> char_queue{};
+bool quit{ false };
+
+void add_chars_to_queue()
 {
-    wprintf((SomeBool ? L"true\n" : L"false\n"));
-    wprintf(L"Press any button to continue"); _getch();
-    return;
+    wchar_t c{};
+    for (;;)
+    {
+        c = static_cast<char>(_getch());
+        if (!std::wcin)
+        {
+            std::unique_lock<std::mutex> lck{ mtx };
+            quit = true;
+            cv.notify_all();
+            return;
+        }
+        if (c == L'q' || c == L'Q')
+        {
+            std::unique_lock<std::mutex> lck{ mtx };
+            quit = true;
+            char_queue.push(c);
+            cv.notify_all();
+            return;
+        }
+        if (c == '\n')
+            continue;
+        std::unique_lock<std::mutex> lck{ mtx };
+        char_queue.push(c);
+        cv.notify_all();
+    }
 }
 
-void CheckNumber()
+
+std::wstring get_key_or_wait(std::chrono::system_clock::duration d)
 {
-    wprintf((std::to_wstring(number) + L"\n").c_str());
-    wprintf(L"Press any button to continue"); _getch();
-    return;
+    std::unique_lock<std::mutex> lck{ mtx };
+    for (int i{ 10 }; i > 0; --i)
+    {
+        MSG msg;
+        GetMessage(&msg, 0, 0, 0);
+
+        cv.wait_for(lck, d / 10., []() { return quit || !char_queue.empty(); });
+        if (!char_queue.empty())
+            break;
+        if (quit)
+            return{ L"Quitting.\n" };
+        std::wcout << L"Countdown at " << i << '\n';
+    }
+    std::wstring return_string{};
+    if (!char_queue.empty())
+    {
+        return_string += L"Obtained a character from the stream before the timer ran out. Character was: ";
+        return_string += char_queue.front();
+        char_queue.pop();
+    }
+    else
+    {
+        return_string = L"Timer ran out.";
+    }
+
+    return return_string;
 }
 
-void SomeFunction(int* param1, int* param2)
+void PrintingFunction(std::wstring input)
 {
-    std::wcout << L"Param1: " << *param1 << L" | Param2: " << *param2 << std::endl;
-    wprintf(L"Press any button to continue"); _getch();
-    return;
+    wprintf(input.c_str());
 }
 
 int main()
@@ -44,31 +98,37 @@ int main()
     NosStdLib::Console::InitializeModifiers::BeatifyConsole<wchar_t>(L"Mouse Tracking");
     NosStdLib::Console::InitializeModifiers::InitializeEventHandler();
 
+    NosStdLib::Clickable::Button button1(L"button 1", NosStdLib::Dimention::DimentionsD2(1, 1, 10, 3));
+    NosStdLib::Clickable::Button button2(L"button 2", NosStdLib::Dimention::DimentionsD2(20, 3, 29, 5));
+
+    NosStdLib::Clickable::Button::PrintAllButtons();
+
+    //button1.OnEnterHover = new NosStdLib::Button::Event(new NosStdLib::Functional::FunctionStore<void(std::wstring), std::wstring>(&SomeEventFunction, L"Entered button 1\n"));
+    //button1.OnLeaveHover = new NosStdLib::Button::Event(new NosStdLib::Functional::FunctionStore<void(std::wstring), std::wstring>(&SomeEventFunction, L"Left button 1\n"));
+    button1.OnClick = new NosStdLib::Event(new NosStdLib::Functional::FunctionStore<void(std::wstring), std::wstring>(&PrintingFunction, L"Clicked button 1\n"));
+
+    //button2.OnEnterHover = new NosStdLib::Button::Event(new NosStdLib::Functional::FunctionStore<void(std::wstring), std::wstring>(&SomeEventFunction, L"Entered button 2\n"));
+    //button2.OnLeaveHover = new NosStdLib::Button::Event(new NosStdLib::Functional::FunctionStore<void(std::wstring), std::wstring>(&SomeEventFunction, L"Left button 2\n"));
+    button2.OnClick = new NosStdLib::Event(new NosStdLib::Functional::FunctionStore<void(std::wstring), std::wstring>(&PrintingFunction, L"Clicked button 2\n"));
+
     NosStdLib::MouseTracking::InitializeMouseTracking();
 
-    NosStdLib::Menu::DynamicMenu MainMenu(L"Main Menu", true, true, true);
-    NosStdLib::Menu::DynamicMenu SecondaryMenu(L"Second Menu", true, true, true);
 
-    SecondaryMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"Number", &number));
-    SecondaryMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"Check Number", new NosStdLib::Functional::FunctionStore(&CheckNumber)));
+    std::thread get_chars{ []() { add_chars_to_queue(); } };
 
-    int param1 = 0,
-        param2 = 10;
+    std::wcout << L"Type q to exit.\n";
+    for (int i{}; i < 3; ++i)
+    {
+        {
+            std::lock_guard<std::mutex> lck{ mtx };
+            if (quit)
+                break;
+        }
+        std::wcout << L"Waiting for key press followed by <enter>.\n";
+        std::wcout << get_key_or_wait(std::chrono::seconds(10)) << '\n';
+    }
 
-    SecondaryMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"param1", &param1));
-    SecondaryMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"param2", &param2));
-
-    SecondaryMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"Run Function", new NosStdLib::Functional::FunctionStore(&SomeFunction, &param1, &param2)));
-
-    MainMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"Another Menu", &SecondaryMenu));
-
-    MainMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"Toggle", &SomeBool));
-    MainMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"Check Bool", new NosStdLib::Functional::FunctionStore(&CheckBool)));
-
-    MainMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"Number", &number));
-    MainMenu.AddMenuEntry(new NosStdLib::Menu::MenuEntry(L"Check Number", new NosStdLib::Functional::FunctionStore(&CheckNumber)));
-
-    MainMenu.StartMenu();
+    get_chars.join();
 
     wprintf(L"Press any button to continue"); _getch();
     return 0;
