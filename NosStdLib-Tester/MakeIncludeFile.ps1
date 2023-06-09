fb4a0bfef4a2645b81e92f7a622d4e73ee81daed
Adam Krawczyk <Adam882200@gmail.com>
2023-06-09 12:05:28 +0100

    Created script which makes a file with all headers for testing

---
+++ NosStdLib-Tester/MakeIncludeFile.ps1
@@ -0,0 +1,15 @@
+$HEADER = "AllHeaders.hpp"
+
+If (test-path $HEADER)
+{
+    Remove-Item $HEADER
+}
+
+"#ifndef __ALL_HEADERS__" >> $HEADER
+"#define __ALL_HEADERS__" >> $HEADER
+$files = Get-ChildItem "..\NosStdLib\NosStdLib\" -Filter *.hpp
+foreach ($f in $files)
+{
+    "#include <$f>" >> $HEADER
+}
+"#endif" >> $HEADER
