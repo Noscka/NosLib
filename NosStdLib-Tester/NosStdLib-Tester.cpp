8b4352a242aa5c56655a387b8e701502702213cc
Adam <adam882200@gmail.com>
2023-06-15 23:25:44 +0100

    Created a basic _getch user input gather function

--- NosStdLib-Tester/NosStdLib-Tester.cpp
+++ NosStdLib-Tester/NosStdLib-Tester.cpp
@@ -11,6 +11,7 @@
 std::wstring GetUserInput() /* TESTING */
 {
     std::wstring output;
+    unsigned int negativeOffset = 0;
 
     while (true)
     {
@@ -18,7 +19,27 @@ std::wstring GetUserInput() /* TESTING */
 
         if (ch == NosStdLib::Definitions::ENTER)
         {
+            wprintf(L"\n");
             break; /* Finish gathering */
+        }
+        else if (ch == NosStdLib::Definitions::BACKSPACE)
+        {
+			// Coord for backspace cursor position editing
+			COORD NewCoord;
+            CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+
+			if (!output.empty())
+			{
+                NosStdLib::Console::GetConsoleSize(GetStdHandle(STD_OUTPUT_HANDLE), &consoleInfo);
+
+				NewCoord = {(SHORT)(consoleInfo.dwCursorPosition.X - 1), consoleInfo.dwCursorPosition.Y}; // create new coord with x-1 and same y
+				SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), NewCoord); // use new coord
+				wprintf(L" "); // delete character
+				SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), NewCoord);
+                output.pop_back();
+			}
+
+            continue; /* skip printing and adding character to string */
         }
         else if (!(ch && ch != 224))
         {
@@ -27,13 +48,24 @@ std::wstring GetUserInput() /* TESTING */
             switch (exCh)
             {
             case NosStdLib::Definitions::ARROW_LEFT:
-                break;
+                if (negativeOffset < output.size()) /* only if less then the size of the current string */
+                {
+                    negativeOffset++; /* increase backwards offset */
+                }
+                continue; /* Don't process character */
+
             case NosStdLib::Definitions::ARROW_RIGHT:
-                break;
+				if (negativeOffset > 0) /* only if offset is more then 0 */
+                {
+                    negativeOffset--;
+                }
+				continue; /* Don't process character */
             }
         }
 
-        output.push_back(ch);
+        wprintf(std::format(L"{}", (wchar_t)ch).c_str());
+        output.insert(output.end() - negativeOffset, (wchar_t)ch);
+
     }
 
     return output;
@@ -50,8 +82,13 @@ int main()
 
     wprintf(L"quick input: ");
     getline(std::wcin, output);
-    wprintf(std::format(L"{}\n", output).c_str());
-    wprintf(std::format(L"size: {}\n", output.size()).c_str());
+	wprintf(L"%s\n", output.c_str());
+	wprintf(L"%zd\n", output.size());
+
+	wprintf(L"quick input: ");
+    output = GetUserInput();
+    wprintf(L"%s\n", output.c_str());
+    wprintf(L"%zd\n", output.size());
 
     wprintf(L"Press any button to continue"); _getch();
 	return 0;
