32b1deed6f8ad7974fd5aff8c3e90e92b8b13c5a
Adam <adam882200@gmail.com>
2023-07-01 16:11:52 +0100

    Testing `GetScrollInfo` and ANSI mouse positioning, ANSI can only move to inside viewport (not useful in this case)

--- NosLib-Tester/NosLib-Tester.cpp
+++ NosLib-Tester/NosLib-Tester.cpp
@@ -10,10 +10,34 @@ int main()
 {
     NosLib::Console::InitializeModifiers::EnableUnicode();
     NosLib::Console::InitializeModifiers::EnableANSI();
-    NosLib::Console::InitializeModifiers::BeatifyConsole<wchar_t>(L"Dynamic Array Updating");
+    //NosLib::Console::InitializeModifiers::BeatifyConsole<wchar_t>(L"Dynamic Array Updating");
     NosLib::Console::InitializeModifiers::InitializeEventHandler();
 
-	int variable1 = 0;
+	wprintf(L"Press any button to continue"); _getch();
+
+	SCROLLINFO info;
+	info.cbSize = sizeof(SCROLLINFO);
+	info.fMask = SIF_ALL;
+
+	GetScrollInfo(GetConsoleWindow(), SB_VERT, &info);
+
+	wprintf(L"\033[%d;0f", (SHORT)(info.nPos));
+	wprintf(L"Virtual\n");
+	wprintf(L"Max		- %d\n", info.nMax);
+	wprintf(L"Min		- %d\n", info.nMin);
+	wprintf(L"Page		- %d\n", info.nPage);
+	wprintf(L"Pos		- %d\n", info.nPos);
+	wprintf(L"TrackPos	- %d\n", info.nTrackPos);
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {0, (SHORT)(info.nPos)});
+	wprintf(L"API\n");
+	wprintf(L"Max		- %d\n", info.nMax);
+	wprintf(L"Min		- %d\n", info.nMin);
+	wprintf(L"Page		- %d\n", info.nPage);
+	wprintf(L"Pos		- %d\n", info.nPos);
+	wprintf(L"TrackPos	- %d\n", info.nTrackPos);
+
+	/*int variable1 = 0;
 
 	NosLib::Menu::DynamicMenu mainMenu(L"Basic Menu");
 
@@ -26,7 +50,7 @@ int main()
 		mainMenu.AddMenuEntry(new NosLib::Menu::MenuEntry<int>(std::format(L"int {}", i), &variable1));
 	}
 
-	mainMenu.StartMenu();
+	mainMenu.StartMenu();*/
 
     wprintf(L"Press any button to continue"); _getch();
     return 0;
