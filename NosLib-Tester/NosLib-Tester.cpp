f1e9eb5602b905071a382c0f997007fd348422ba
Adam <adam882200@gmail.com>
2023-08-18 01:11:13 +0100

    h

--- NosLib-Tester/NosLib-Tester.cpp
+++ NosLib-Tester/NosLib-Tester.cpp
@@ -1,10 +1,12 @@
 ﻿#include "NosLib/Console.hpp"
 #include "NosLib/String.hpp"
+#include "NosLib/Logging.hpp"
+#include "NosLib/ErrorHandling.hpp"
 
-#include "AllHeaders.hpp"
-
-#include <Windows.h>
+#include <windows.h>
 #include <conio.h>
+#include <cstddef>
+#include <cassert>
 
 /* TODO: Figure out if it is worth it to change calling convention from default (__cdelc) to __fastcall */
 
@@ -12,15 +14,8 @@ int main()
 {
 	NosLib::Console::InitializeModifiers::EnableUnicode();
 	NosLib::Console::InitializeModifiers::EnableANSI();
-	NosLib::Console::InitializeModifiers::BeatifyConsole<wchar_t>(L"File Shortcut Testing");
+	NosLib::Console::InitializeModifiers::BeatifyConsole<wchar_t>(L"Nothing");
 	NosLib::Console::InitializeModifiers::InitializeEventHandler();
-
-	NosLib::DynamicArray<std::wstring> basicArray({L"d", L"e", L"f", L"c"});
-	NosLib::DynamicArray<std::wstring> secondArray = NosLib::DynamicArray<std::wstring>({L"a", L"b", L"c"}) + basicArray.ObjectExclude(L"d");
-	NosLib::DynamicArray<std::wstring> copyObjectArray = basicArray.ObjectExclude(L"b");
-	NosLib::DynamicArray<std::wstring> copyIndexArray = basicArray.Exclude(1);
-
-	std::wcout << secondArray << std::endl;
 
 	wprintf(L"Press any button to continue"); _getch();
 	return 0;
