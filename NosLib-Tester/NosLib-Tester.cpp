53ae820c58a34bfe5afec892dc8c1ed276e03ffa
Adam Krawczyk <Adam882200@gmail.com>
2023-09-13 15:18:38 +0100

    Testing Flags

--- NosLib-Tester/NosLib-Tester.cpp
+++ NosLib-Tester/NosLib-Tester.cpp
@@ -8,54 +8,43 @@
 
 /* TODO: Figure out if it is worth it to change calling convention from default (__cdelc) to __fastcall */
 
-class SimpleTestClass
+enum FlagTesting
 {
-protected:
-	int Num1;
-	int Num2;
-	std::wstring Name;
-public:
-	SimpleTestClass(){}
-
-	SimpleTestClass(const int& num1, const int& num2, const std::wstring& name)
-	{
-		Num1 = num1;
-		Num2 = num2;
-		Name = name;
-	}
-
-	std::wstring GetKey()
-	{
-		return Name;
-	}
+	Flag1 = 1 << 0,
+	Flag2 = 1 << 1,
+	Flag3 = 1 << 2,
+	Flag4 = 1 << 3,
 };
+DEFINE_ENUM_FLAG_OPERATORS(FlagTesting)
 
 int main()
 {
 	NosLib::Console::InitializeModifiers::EnableUnicode();
 	NosLib::Console::InitializeModifiers::EnableANSI();
-	NosLib::Console::InitializeModifiers::BeatifyConsole<wchar_t>(L"Hash Table");
+	NosLib::Console::InitializeModifiers::BeatifyConsole<wchar_t>(L"Basic Flag Testing");
 	NosLib::Console::InitializeModifiers::InitializeEventHandler();
 
-	NosLib::HashTable<std::wstring, SimpleTestClass**> testHashTable(&SimpleTestClass::GetKey);
+	FlagTesting flag = Flag1 | Flag2 | Flag3;
 
-	SimpleTestClass* megaPointer1 = new SimpleTestClass(1, 2, L"NiceName");
-	SimpleTestClass** megaPointer2 = &megaPointer1;
-
-	testHashTable.Insert(megaPointer2);
-
-	SimpleTestClass*** object = testHashTable.Find(L"NiceName");
-
-	if (object == nullptr)
+	if (flag & Flag1)
 	{
-		wprintf(L"Not found\n");
-	}
-	else
-	{
-		wprintf(L"found object\n");
+		wprintf(L"Contains Flag1\n");
 	}
 
-	wprintf((testHashTable.Remove(L"NiceName") ? L"Removed Successfully\n" : L"Not Removed\n"));
+	if (flag & Flag2)
+	{
+		wprintf(L"Contains Flag2\n");
+	}
+
+	if (flag & Flag3)
+	{
+		wprintf(L"Contains Flag3\n");
+	}
+
+	if (flag & Flag4)
+	{
+		wprintf(L"Contains Flag4\n");
+	}
 
 	wprintf(L"Press any button to continue"); _getch();
 	return 0;
