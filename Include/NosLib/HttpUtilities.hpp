723fa8d70382bd369a313dd9459aee63e314d699
Adam <adam882200@gmail.com>
2024-06-24 02:49:09 +0100

    Moved HttpClient to HttpUtilities

--- Include/NosLib/HttpUtilities.hpp
+++ Include/NosLib/HttpUtilities.hpp
@@ -1,27 +1,100 @@
 #ifndef _HTTPUTILITIES_NOSLIB_HPP_
 #define _HTTPUTILITIES_NOSLIB_HPP_
 
-#include <httplib.h>
-
-#include "HttpClient.hpp"
+#include "Logging.hpp"
+#include "String.hpp"
 #include "HostPath.hpp"
 
+#include <httplib.h>
+
 #include <fstream>
-#include <mutex>
 
 namespace NosLib
 {
 	namespace HttpUtilities
 	{
+		namespace /* PRIVATE */
+		{
+			inline static std::mutex DownloadMutex;
+			inline static std::string UserAgent;
+		}
+
+		inline void LoggingFunction(const httplib::Request& req, const httplib::Response& res)
+		{
+			/* if is more then Debug */
+			if (NosLib::Logging::GetVerboseLevel() > Logging::Verbose::Debug)
+			{
+				return;
+			}
+
+			std::string logOutput;
+			logOutput += ("====================================================================================================================\nRequest\n");
+			logOutput += (std::format(":METHOD: {}\n", req.method).c_str());
+			logOutput += (std::format(":PATH:   {}\n", req.path).c_str());
+			logOutput += (std::format(":BODY:   {}\n", req.body).c_str());
+
+			logOutput += ("======HEADERS======\n");
+
+			for (auto itr = req.headers.begin(); itr != req.headers.end(); itr++)
+			{
+				logOutput += (std::format("{} : {}\n", itr->first, itr->second).c_str());
+			}
+			logOutput += ("====================================================================================================================\nResponse\n");
+
+			logOutput += (std::format(":STATUS: {}\n", res.status).c_str());
+			logOutput += (std::format(":REASON: {}\n", res.reason).c_str());
+			logOutput += (std::format(":BODY:   {}\n", res.body).c_str());
+			logOutput += (std::format(":LOCATION:   {}\n", res.location).c_str());
+
+			logOutput += ("======HEADERS======\n");
+
+			for (auto itr = res.headers.begin(); itr != res.headers.end(); itr++)
+			{
+				logOutput += (std::format("{} : {}\n", itr->first, itr->second).c_str());
+			}
+			logOutput += ("====================================================================================================================\n\n\n");
+
+			NosLib::Logging::CreateLog<char>(logOutput, NosLib::Logging::Severity::Debug);
+		}
+
+		inline httplib::Client MakeClient(const std::string& host, const bool& enableServerCertVerification = true)
+		{
+			httplib::Client ret(host);
+
+			ret.enable_server_certificate_verification(enableServerCertVerification);
+
+			ret.set_follow_location(false);
+			ret.set_keep_alive(false);
+			ret.set_default_headers({ {"User-Agent", std::format("{} (cpp-httplib)", UserAgent).c_str()} });
+
+			ret.set_logger(&LoggingFunction);
+
+			return ret;
+		}
+
+		inline void SetUserAgent(const std::string& userAgent)
+		{
+			UserAgent = userAgent;
+		}
+
+		inline void SetUserAgent(const std::wstring& userAgent)
+		{
+			UserAgent = NosLib::String::ToString(userAgent);
+		}
+
 		inline bool DownloadFile(httplib::Client* client, const std::string& urlPath, const std::string& filepath)
 		{
 			std::ofstream fileDownloadStream(filepath, std::ios::binary);
 
-			httplib::Result res = client->Get(urlPath, [&](const char* data, size_t data_length)
+			httplib::Result res
+			{
+				std::lock_guard<std::mutex> lock(DownloadMutex);
+				res = client->Get(urlPath, [&](const char* data, size_t data_length)
 				{
 					fileDownloadStream.write(data, data_length);
 					return true;
 				});
+			}
 
 			if (!res)
 			{
